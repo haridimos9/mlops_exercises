3fb2539f1b1073d750c60e57e7349221d9834e0e
haridimos9 <haridimos9@gmail.com>
2023-01-09 12:14:32 +0100

    adding training files

---
+++ src/models/data.py
@@ -0,0 +1,38 @@
+import torch
+import wget
+from torch.utils.data import Dataset
+import numpy as np
+import os
+
+class CorruptMnist(Dataset):
+    
+    def __init__(self, train):
+        path = "data/corruptmnist/"
+        if train:
+            content = [ ]
+            for i in range(5):
+                content.append(np.load(f"{path}train_{i}.npz", allow_pickle=True))
+            data = torch.tensor(np.concatenate([c['images'] for c in content])).reshape(-1, 1, 28, 28)
+            targets = torch.tensor(np.concatenate([c['labels'] for c in content]))
+        else:
+            content = np.load(f"{path}test.npz", allow_pickle=True)
+            data = torch.tensor(content['images']).reshape(-1, 1, 28, 28)
+            targets = torch.tensor(content['labels'])
+            
+        self.data = data
+        self.targets = targets
+    
+    def __len__(self):
+        return self.targets.numel()
+    
+    def __getitem__(self, idx):
+        return self.data[idx].float(), self.targets[idx]
+
+
+if __name__ == "__main__":
+    dataset_train = CorruptMnist(train=True)
+    dataset_test = CorruptMnist(train=False)
+    print(dataset_train.data.shape)
+    print(dataset_train.targets.shape)
+    print(dataset_test.data.shape)
+    print(dataset_test.targets.shape)
